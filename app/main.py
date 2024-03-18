bbb247691236964399e5479a4d35b37429991870
jeremiah-carlson <87202597+jeremiah-carlson@users.noreply.github.com>
2024-03-18 00:19:22 -0400

    Initial test

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import cadquery as cq
 
+assy = cq.Assembly()
 # These can be modified rather than hardcoding values for each dimension.
 length = 80.0  # Length of the block
 height = 60.0  # Height of the block
@@ -10,10 +11,11 @@ thickness = 10.0  # Thickness of the block
 # 1a. Uses the X and Y origins to define the workplane, meaning that the
 # positive Z direction is "up", and the negative Z direction is "down".
 result = cq.Workplane("XY").box(length, height, thickness)
+box1 = cq.Workplane("XY").box(length, height, thickness)
 
 # The following method is now outdated, but can still be used to display the
 # results of the script if you want
 # from Helpers import show
-# show(result)  # Render the result of this script
-
-result.toSvg('test.svg')
+assy.add(result, loc=cq.Location((1,2,3)), color=cq.Color('green'), name='test')
+assy.add(result, loc=cq.Location((100,200,300)), name='test2')
+assy.save('test.step')
